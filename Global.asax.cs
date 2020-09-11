c45bcd088e813697450434a66c9a8fb2ffb78a4a
Niels Swimberghe <nielsswimberghe@gmail.com>
2020-09-11 13:01:20 -0400

    Update Global.asax.cs

--- Global.asax.cs
+++ Global.asax.cs
@@ -20,6 +20,10 @@ namespace AddRawUrlToApplicationInsights
 
         protected void Application_BeginRequest(object sender, EventArgs e)
         {
+            // This code snippet is for demo purposes only!
+            // these types of rewrites can be performed by IIS Rewrite module 
+            // or you can update the routing table to resolve '/' to the HomeController
+            
             string fullOrigionalpath = Request.Url.ToString();
             if (fullOrigionalpath.Contains("/about"))
             {
