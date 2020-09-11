17b7c7cc869be6d1eb82027817fe25638fc5d9b7
Niels Swimberghe <nielsswimberghe@gmail.com>
2020-09-11 11:57:14 -0400

    Add project files.

---
+++ Global.asax.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace AddRawUrlToApplicationInsights
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            string fullOrigionalpath = Request.Url.ToString();
+            if (fullOrigionalpath.Contains("/about"))
+            {
+                Context.RewritePath("/home/about");
+            }
+            else if(fullOrigionalpath.Contains("/contact"))
+            {
+                Context.RewritePath("/home/contact");
+            }
+        }
+    }
+}
---
+++ RawUrlTelemetryInitializer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Web;
+using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.ApplicationInsights.Extensibility;
+
+namespace AddRawUrlToApplicationInsights
+{
+    public class RawUrlTelemetryInitializer : ITelemetryInitializer
+    {
+        public void Initialize(ITelemetry telemetry)
+        {
+            if (telemetry is RequestTelemetry requestTelemetry)
+            {
+                var request = HttpContext.Current.Request;
+                requestTelemetry.Properties["RawUrl"] = request.RawUrl;
+                requestTelemetry.Properties["RawUrlFqdn"] = new Uri(request.Url, request.RawUrl).ToString();
+            }
+        }
+    }
+}
